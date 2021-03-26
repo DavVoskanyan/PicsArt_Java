7805cf469360f2a466977b5960b7d0c2ab3904a5
DavVoskanyan <81379810+DavVoskanyan@users.noreply.github.com>
2021-03-26 23:08:02 +0400

    Create primitiveVariables.java
    Here are writen all 8 primitive types of Java and they are printed in console.

---
+++ primitiveVariables.java
@@ -0,0 +1,31 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        byte byteTypeVar = -18;
+        System.out.println(byteTypeVar);
+
+        short shortTypeVar = 100;
+        System.out.println(shortTypeVar);
+
+        int intTypeVar;
+        intTypeVar = 237;
+        System.out.println(intTypeVar);
+
+        long longTypeVar = 12345678910l;
+        System.out.println(longTypeVar);
+
+        double doubleNum = 0.4632;
+        System.out.println(doubleNum);
+
+        float floatNum = 5.15646543f;
+        System.out.println(floatNum);
+
+        char justOneSymbol = 'S';
+        System.out.println(justOneSymbol);
+
+        boolean boolTypeVar;
+        boolTypeVar = false;
+        System.out.println(boolTypeVar);
+
+        System.out.println("These are all primitive types of variables");
+    }
+}
