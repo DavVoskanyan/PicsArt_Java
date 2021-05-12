5060c0171f4c0d629769bbb3518a138ee7a020aa
DavVoskanyan <81379810+DavVoskanyan@users.noreply.github.com>
2021-05-12 22:29:16 +0400

    Delete OOP_project directory

--- OOP_project/mainCookClass/CookInterface.java
+++
@@ -1,7 +0,0 @@
-package mainCookClass;
-
-public interface CookInterface {
-    void makeBreakfast();
-    void makeDinner();
-    void makeSupper();
-}
--- OOP_project/mainCookClass/SubCookClass/ChefsInterface.java
+++
@@ -1,7 +0,0 @@
-package mainCookClass.SubCookClass;
-
-public interface ChefsInterface {
-    void makingDish1();
-    void makingDish2();
-    void makingDish3();
-}
